ca80571cceb098404cb330f6996b3150e576c2e5
Carlos Gabaldon <cgabaldon@gmail.com>
2012-07-08 16:12:30 -0700

    Added cheat sheets for capybara, git, and rspec

--- rakefile.rb
+++ rakefile.rb
@@ -1,11 +1,22 @@
 require 'yaml'
 
 def p command, description
-  puts "#{command} => #{description}."
+  puts " #{command} # #{description}."
 end
 
-
 namespace :cheater do
+  
+  task :default => [:docs]
+  
+  task :docs do
+    files = Dir.glob("*.yml")
+    puts "Available Cheet Sheets"
+    files.each do |file|
+      command = "rake #{file.gsub(/.yml/, '').tr('_', ':')}"
+      desc = "Cheat Sheet for #{command.gsub(/rake cheater:/, '')}"
+      p command, desc
+    end
+  end
   
   rule "" do |t|
     command_file = "#{t.name.tr(":", "_")}.yml"
